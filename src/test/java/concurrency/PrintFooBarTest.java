fae8aa7b7f8a1698e5c14516607e7abf291683a7
Jon Vallet <j.vallet@gmail.com>
2025-08-17 10:59:38 +0100

    Implement PrintFooBar (#4)

---
+++ src/test/java/concurrency/PrintFooBarTest.java
@@ -0,0 +1,20 @@
+package concurrency;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class PrintFooBarTest {
+    @Test
+    void printFooBar() {
+        var n = 5;
+        var printFooBar = new PrintFooBar(n);
+        try (ExecutorService service = Executors.newFixedThreadPool(2)) {
+            service.submit(() -> printFooBar.foo(() -> System.out.println("foo")));
+            service.submit(() -> printFooBar.bar(() -> System.out.println("bar")));
+        }
+
+    }
+
+}
